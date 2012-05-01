75edbedd7b989222321cc9bbfae5e7346e201020
David Dyess <david.dyess@gmail.com>
2012-05-01 17:37:24 -0500

    Copyright update

--- html/index.php
+++ html/index.php
@@ -2,16 +2,11 @@
 /**
  * Clay Framework
  *
- * @copyright (C) 2007-2011 David L Dyess II
+ * @copyright (C) 2007-2012 David L Dyess II
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://clay-project.com
  * @author David L Dyess II (david.dyess@gmail.com)
  */
-
-	/* Clay Framework - Copyright Invenotech LLC 2008
-	 * Author: David Dyess II
-	 * All Rights Reserved
-	*/
 
 	$starttimer = time()+microtime();
 	//ini_set('zlib.output_compression', 'on');
